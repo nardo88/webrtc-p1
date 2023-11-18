0dc263f132abfef93a27872c8546a8d27dfc6627
nardo88 <nardo1988@mail.ru>
2023-11-18 18:24:05 +0300

    end

--- main.js
+++ main.js
@@ -23,6 +23,14 @@ const servers = {
   ],
 };
 
+const constraints = {
+  video: {
+    with: { min: 640, ideal: 1920, max: 1920 },
+    height: { min: 480, ideal: 1080, max: 1080 },
+  },
+  audio: true,
+};
+
 const init = async () => {
   client = await AgoraRTM.createInstance(APP_ID);
   await client.login({ uid, token });
@@ -35,10 +43,7 @@ const init = async () => {
 
   client.on("MessageFromPeer", handleMessageFromPeer);
 
-  localStream = await navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: true,
-  });
+  localStream = await navigator.mediaDevices.getUserMedia(constraints);
 
   document.getElementById("user-1").srcObject = localStream;
 };
@@ -49,6 +54,8 @@ const handleUserJoined = async (MemberId) => {
 
 const handleUserLeft = async (MemberId) => {
   document.getElementById("user-2").style.display = "none";
+
+  document.getElementById("user-1").classList.remove("smallFrame");
 };
 
 const createPeerConnection = async (MemberId) => {
@@ -58,11 +65,10 @@ const createPeerConnection = async (MemberId) => {
   document.getElementById("user-2").srcObject = remoteStream;
   document.getElementById("user-2").style.display = "block";
 
+  document.getElementById("user-1").classList.add("smallFrame");
+
   if (!localStream) {
-    localStream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: true,
-    });
+    localStream = await navigator.mediaDevices.getUserMedia(constraints);
 
     document.getElementById("user-1").srcObject = localStream;
   }
@@ -160,8 +166,21 @@ const toggleCamera = () => {
   }
 };
 
-// 1:19:23
+const toggleMic = () => {
+  const audioTrack = localStream
+    .getTracks()
+    .find((track) => track.kind === "audio");
+  if (audioTrack.enabled) {
+    audioTrack.enabled = false;
+    document.getElementById("mic-btn").style.backgroundColor = "rgb(255,80,80)";
+  } else {
+    audioTrack.enabled = true;
+    document.getElementById("mic-btn").style.backgroundColor =
+      "rgb(179, 102, 249, 0.9)";
+  }
+};
 
 document.getElementById("camera-btn").addEventListener("click", toggleCamera);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
 
 init();
